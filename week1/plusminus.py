83d51ca9a1dd9a506890e5e1a3b4093f97600fb5
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-03-02 23:07:01 +0530

    Initial Commit

--- week1/plusminus.py
+++ week1/plusminus.py
@@ -1,10 +1,9 @@
 #Question 
 #Answer
 def calculate_ratios(arr):
-    pos=0     # pos=count of +ve no
-    neg=0    # neg=count of -ve no
-    zero=0     # zero=count of 0s
-# check no. of positive negative and zero present in array
+    pos=0     
+    neg=0    
+    zero=0     
     for num in arr:
         if num > 0:
             pos += 1
@@ -12,7 +11,7 @@ def calculate_ratios(arr):
             neg += 1
         else:
             zero += 1
-# n is total no. of element
+
     n = len(arr)
     pos_r = pos / n
     neg_r= neg/ n
