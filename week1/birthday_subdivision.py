1631b538d3c5cf241d86bbc3292a797e544024a1
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-03-29 07:35:41 +0530

    Initial Commit

--- week1/birthday_subdivision.py
+++ week1/birthday_subdivision.py
@@ -4,7 +4,6 @@ def birthday(s, d, m):
         if sum(s[i:i + m]) == d:
             count += 1
     return count
-
 if __name__ == "__main__":
     n = int(input())
     s = list(map(int, input().split()))
