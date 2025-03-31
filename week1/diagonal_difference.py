b722984676d6a6d1cbd18721a779a409dcedf2d8
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-03-31 22:59:11 +0530

    Initial Commit

--- week1/diagonal_difference.py
+++ week1/diagonal_difference.py
@@ -2,7 +2,6 @@ def diagonalDifference(arr):
     n = len(arr)
     primary_diagonal_sum = 0
     secondary_diagonal_sum = 0
-    
     for i in range(n):
         primary_diagonal_sum += arr[i][i]
         secondary_diagonal_sum += arr[i][n - 1 - i]
