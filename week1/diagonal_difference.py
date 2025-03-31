8904b12831172472620f6441a14c3066fc5d692a
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-03-31 22:59:58 +0530

    Initial Commit

--- week1/diagonal_difference.py
+++ week1/diagonal_difference.py
@@ -6,7 +6,6 @@ def diagonalDifference(arr):
         primary_diagonal_sum += arr[i][i]
         secondary_diagonal_sum += arr[i][n - 1 - i]
     return abs(primary_diagonal_sum - secondary_diagonal_sum)
-
 if __name__ == "__main__":
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
