1883bd654488ff81a070e0af6a962e8765abb2a8
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-04-01 22:04:34 +0530

    Initial Commit

--- week1/lonelyInteger.py
+++ week1/lonelyInteger.py
@@ -3,6 +3,7 @@ def lonelyinteger(a):
     for number in a:
         unique ^= number
     return unique
+    
 if __name__ == "__main__":
     n = int(input())
     a = list(map(int, input().strip().split()))
