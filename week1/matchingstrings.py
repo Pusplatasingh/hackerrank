be443373e505c7006f65ae726d3acf5ece95fea8
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-04-01 22:02:01 +0530

    Initial Commit

--- week1/matchingstrings.py
+++ week1/matchingstrings.py
@@ -1,7 +1,6 @@
 def matchingStrings(strings, queries):
     string_count = {s: strings.count(s) for s in set(strings)}
     return [string_count.get(query, 0) for query in queries]
-
 if __name__ == "__main__":
     n = int(input())
     strings = [input().strip() for _ in range(n)]
