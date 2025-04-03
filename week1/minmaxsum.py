5290bfb682bd145e46e85e9d03594c3d5a8c79b4
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-04-03 20:27:03 +0530

    Initial Commit

--- week1/minmaxsum.py
+++ week1/minmaxsum.py
@@ -6,7 +6,6 @@ def miniMaxSum(arr):
     
     min_sum = total_sum - max_value
     max_sum = total_sum - min_value
-
     print(min_sum, max_sum)
 input_data = input()  
 arr = list(map(int, input_data.split())) 
