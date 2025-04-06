87a7f59113a853f3ef44fe086ab6df92b286e64e
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-04-06 21:43:43 +0530

    Initial Commit

--- week1/twoarrays.py
+++ week1/twoarrays.py
@@ -1,7 +1,7 @@
 def twoArrays(k, A, B):
     A.sort()
     B.sort(reverse=True)
-    return "YES" if all(a + b >= k for a, b in zip(A, B)) else "NO"
+    return "Yes" if all(a + b >= k for a, b in zip(A, B)) else "NO"
 if __name__ == "__main__":
     q = int(input())
     for _ in range(q):
