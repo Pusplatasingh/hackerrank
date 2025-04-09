718af1e5cb71ad624bceae2f9db0b7eb6b68c078
Pusplatasingh <162028658+Pusplatasingh@users.noreply.github.com>
2025-04-09 21:38:04 +0530

    Initial Commit

--- week2/sales_market.py
+++ week2/sales_market.py
@@ -1,6 +1,6 @@
 def sockMerchant(n, ar):
     from collections import Counter
-    color_= Counter(ar)
+    color_count = Counter(ar)
     return sum(count // 2 for count in color_count.values())
 if __name__ == "__main__":
     n = int(input())
